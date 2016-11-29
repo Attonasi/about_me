a0ce1aeb7fb7818aa2be51088e9b1d3bca255759
Ophelia Yin <owyowy1@gmail.com>
2016-11-29 14:58:07 -0800

    added question/response

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-alert('I AM ALIVE!!!');
+'use strict';
+
+// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+alert('Welcome to about_me guessing game, please reply each question with Y\/N or YES\/NO')
+
+var ans1 = prompt('Does Sam have any cats? The answer must be either ').toUpperCase();
+
+if (ans1 == "Y" || ans1 == "YES") {
+  alert('Yes, Sam does have three cats!');
+} else if (asn1 == "N" || ans1 == "NO") {
+  alert('Sorry, you\'re wrong, Sam has three cats.');
+} else {
+  alert('Sorry, the answer must be either Y\/N or YES\/NO. We\'ll move on to another question.');
+}
+
+console.log(ans1);
+
+var ans2 = prompt('Is Sam a teacher? The answer must be either ').toUpperCase();
+
+if (ans2 == "Y" || ans2 == "YES") {
+  alert('Yes, Sam teaches Code Fellows 201!');
+} else if (ans2 == "N" || ans2 == "NO") {
+  alert('Sorry, you\'re wrong, Sam teaches Code Fellows 201.');
+} else {
+  alert('Sorry, the answer must be either Y\/N or YES\/NO. We\'ll move on to another question.');
+}
+
+console.log(ans2);
+
+var ans3 = prompt('Is Sam a teacher? The answer must be either ').toUpperCase();
+
+if (ans3 == "Y" || ans3 == "YES") {
+  alert('Yes, Sam teaches Code Fellows 201!');
+} else if (ans3 == "N" || ans3 == "NO") {
+  alert('Sorry, you\'re wrong, Sam teaches Code Fellows 201.');
+} else {
+  alert('Sorry, the answer must be either Y\/N or YES\/NO. We\'ll move on to another question.');
+}
+
+console.log(ans3);
+
+var ans4 = prompt('Is Sam a teacher? The answer must be either ').toUpperCase();
+
+if (ans4 == "Y" || ans4 == "YES") {
+  alert('Yes, Sam teaches Code Fellows 201!');
+} else if (ans4 == "N" || ans4 == "NO") {
+  alert('Sorry, you\'re wrong, Sam teaches Code Fellows 201.');
+} else {
+  alert('Sorry, the answer must be either Y\/N or YES\/NO. We\'ll move on to another question.');
+}
+
+console.log(ans4);
+
+var ans5 = prompt('Is Sam a teacher? The answer must be either ').toUpperCase();
+
+if (ans5 == "Y" || ans5 == "YES") {
+  alert('Yes, Sam teaches Code Fellows 201!');
+} else if (ans5 == "N" || ans5 == "NO") {
+  alert('Sorry, you\'re wrong, Sam teaches Code Fellows 201.');
+} else {
+  alert('Sorry, the answer must be either Y\/N or YES\/NO. We\'ll move on to another question.');
+}
+
+console.log(ans5);
